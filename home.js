78f265e756b28ee5f8db4dbe09391398ec083d52
Saumel-Das <saumel.spr@gmail.com>
2026-03-08 11:10:39 +0600

    modal added

--- home.js
+++ home.js
@@ -13,21 +13,26 @@ const loadAllissues = async () => {
     const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
     const data = await res.json();
     alldata = data.data;
+    // console.log(data, alldata);
     displayissues(alldata);
 }
 loadAllissues();
 const issueModalCard = document.getElementById('issueModal');
-const issueModal = (issue) =>{
-
-   issueModalCard.innerHTML = `
+const issueModal = async (id) => {
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+    const temp = await res.json();
+    const issue = temp.data;
+    // console.log(issue.data);
+    
+    issueModalCard.innerHTML = `
    <div class="modal-box px-8 py-8">
-            <h3 class="text-2xl font-bold">Fix broken image uploads</h3>
+            <h3 class="text-2xl font-bold">${issue.title}</h3>
             <div class="flex gap-2 items-center mt-3">
-                <div class="badge badge-success">Success</div>
+                <div class="badge badge-success">${issue.status}</div>
                 <div class="h-1 w-1 bg-gray-500 rounded-full"></div>
-                <div class="text-[12px] text-[#64748B]">Opened by Fahim Ahmed</div>
+                <div class="text-[12px] text-[#64748B]">Opened by </div>
                 <div class="h-1 w-1 bg-gray-500 rounded-full"></div>
-                <div class="text-[12px] text-[#64748B]">22/02/2026</div>
+                <div class="text-[12px] text-[#64748B]">${issue.updatedAt}</div>
             </div>
             <div class="card-actions justify-start mt-5">
                 <div class="badge badge-error badge-soft font-semibold"><img src="assets/BugDroid.png" alt="">Bug</div>
@@ -36,12 +41,11 @@ const issueModal = (issue) =>{
                 </div>
             </div>
 
-            <p class="line-clamp-2 py-5 text-[#64748B]">The navigation menu doesn't collapse properly on mobile devices.
-                Need to fix the responsive behavior.</p>
+            <p class="line-clamp-2 py-5 text-[#64748B]">${issue.description}</p>
             <div class="flex bg-base-200 p-4 gap-40 rounded-md">
                 <div>
                     <p>Assignee:</p>
-                    <p>Fahim Ahmed</p>
+                    <p>${issue.assignee}</p>
                 </div>
                 <div>
                     <p>Priority:</p>
@@ -55,7 +59,7 @@ const issueModal = (issue) =>{
                 </form>
             </div>
         </div>`
-   issueModalCard.showModal();
+    issueModalCard.showModal();
 }
 const displayissues = (issues) => {
     const displayContainer = document.getElementById('issue-container');
@@ -70,13 +74,13 @@ const displayissues = (issues) => {
     })
 
     filteredIssues.forEach(issue => {
-        const statusImg = issue.status === 'open'? 'assets/Open-Status.png' : 'assets/Closed-status.png';
-        const priority = issue.priority === 'high'? 'badge-error':
-                        issue.priority === 'medium'? 'badge-warning':
-                        'badge-ghost text-[#9CA3AF]';
+        const statusImg = issue.status === 'open' ? 'assets/Open-Status.png' : 'assets/Closed-status.png';
+        const priority = issue.priority === 'high' ? 'badge-error' :
+            issue.priority === 'medium' ? 'badge-warning' :
+                'badge-ghost text-[#9CA3AF]';
         const newDiv = document.createElement('div');
         newDiv.innerHTML = `
-        <div class="card bg-base-100 w-full h-[350px] md:h-[300px] shadow-sm" onclick="issueModal(${issue})">
+        <div class="card bg-base-100 w-full h-[350px] md:h-[300px] shadow-sm" onclick="issueModal(${issue.id})">
                 <!-- status and badge -->
                 <div class="flex justify-between px-7 mt-6">
                     <img src="${statusImg}"  alt="">
